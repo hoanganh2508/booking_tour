13e2625455b8fa51871bd0a17fb9c74d4a6fb0e1
binh <khongancungbeo1994@gmail.com>
2021-12-02 20:32:15 +0700

    login logout xong

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def create
+   @user = User.new(user_params)
+   if @user.save
+     flash[:notice] = "User created."
+     redirect_to root_path
+   else
+     render 'new'
+   end
+
+   def edit
+    if logged_in?
+      link to logout path
+    else
+      link to login path
+    end
+   end
+ end
+
+
+private
+
+ def user_params
+   params.require(:user).permit(:name, :email, :password)
+ end
+end
