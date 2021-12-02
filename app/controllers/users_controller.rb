2c2ae7297dc35ab24d86dbf3809a805386c094dd
binh <khongancungbeo1994@gmail.com>
2021-12-02 20:44:27 +0700

    em sua lai r

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,24 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
+
   def create
-   @user = User.new(user_params)
-   if @user.save
-     flash[:notice] = "User created."
-     redirect_to root_path
-   else
-     render 'new'
-   end
-
-   def edit
-    if logged_in?
-      link to logout path
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "User created."
+      redirect_to root_path
     else
-      link to login path
+      render 'new'
     end
-   end
- end
+  end
 
 
-private
+  private
 
- def user_params
-   params.require(:user).permit(:name, :email, :password)
- end
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,7 +24,6 @@ module SessionsHelper
   end
 
   def log_out
-
     session.delete(:user_id)
     @current_user = nil
   end
