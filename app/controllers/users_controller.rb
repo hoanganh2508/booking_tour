f1cde59813fd02527a703b71b9a0977a225c5ce3
hoang anh <hoanganhle30081995@gmail.com>
2021-12-05 19:14:50 +0700

    template sign-up remember-login active-email-login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "User created."
-      redirect_to root_path
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account." flash[:notice] = "User created."
+      redirect_to root_url
     else
       render 'new'
     end
@@ -15,6 +20,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
