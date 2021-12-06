ef39b265f26b4d83f59f8b66f4b0b05763e1cd23
binh <khongancungbeo1994@gmail.com>
2021-12-06 18:58:33 +0700

    sua template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
+  before_action :find_user , only: [:show, :edit, :update, :correct_user]
   before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   def index
     @users = User.paginate(page: params[:page])
   end
+
   def show
-    @user = User.find(params[:id])
   end
+
   def new
     @user = User.new
   end
@@ -27,11 +29,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -44,6 +44,7 @@ end
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
     def logged_in_user
       unless logged_in?
         store_location
@@ -51,8 +52,13 @@ end
         redirect_to login_url
       end
     end
+
+    def find_user
+      @user = User.find_by(id: params[:id])
+      redirect_to root_path unless @user
+    end
+
     def correct_user
-      @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
 end
