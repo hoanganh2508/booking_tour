f746ff048bd84136305acb4bab2fa512294be127
hoang anh <hoanganhle30081995@gmail.com>
2021-12-16 19:26:03 +0700

    manage_users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < Admin::AdminController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :find_user, only: [:edit, :update, :destroy, :show]
 
 
   def index
@@ -7,7 +7,17 @@ class Admin::UsersController < Admin::AdminController
   end
 
   def show
+    redirect_to admin_users_path and return unless @user.activated?
+  end
 
+  def create
+    @user.create
+    flash[:success] = "Tạo tài khoản thành công"
+    redirect_to admin_users_path
+  end
+
+  def new
+    @user = User.new
   end
 
   def edit
@@ -15,15 +25,18 @@ class Admin::UsersController < Admin::AdminController
   end
 
   def update
-    if @user.update[user_params]
-      flash[:success] = "Cập nhật thành công"
-      redirect_to admin_user_path
+    if @user.update(user_params)
+      flash[:success] = "User updated"
+      redirect_to admin_users_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "Đã xóa người dùng"
-    redirect_to admin_user_path
+    redirect_to admin_users_path
   end
 
   private
@@ -32,11 +45,8 @@ class Admin::UsersController < Admin::AdminController
       params.require(:user).permit(:name, :email, :phone_number, :bank_balance, :password, :password_confirmation)
     end
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
+    def find_user
+      @user = User.find_by(id: params[:id])
+      redirect_to admin_users_path unless @user
     end
 end
