ef65248b91d64f03bd2c686286a511a57a1b779b
hoang anh <hoanganhle30081995@gmail.com>
2022-01-15 16:03:56 +0700

    thong ke

--- app/controllers/admin/dash_board_controller.rb
+++ app/controllers/admin/dash_board_controller.rb
@@ -1,5 +1,11 @@
 class Admin::DashBoardController < Admin::AdminController
   def index
-
+    if params[:option]
+      date = "01" + "/" + params[:option] + "/" + Date.today.strftime("%Y")
+      date = date.to_time
+      @user_tours = UserTour.by_dates(date.beginning_of_month, date.end_of_month)
+    else
+      @user_tours = UserTour.by_dates(Time.zone.now.beginning_of_month, Time.zone.now.end_of_month)
+    end
   end
 end
--- app/models/user_tour.rb
+++ app/models/user_tour.rb
@@ -2,5 +2,7 @@ class UserTour < ApplicationRecord
   belongs_to :user
   belongs_to :tour
 
+  scope :by_dates, ->(start_date, end_date) { UserTour.where(created_at: start_date..end_date) }
+
   enum status: {wait:0, confirm:1}
 end
